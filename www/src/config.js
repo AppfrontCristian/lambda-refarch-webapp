d9016b4fed1099d92332a96e17f5a4df7078266a
AppfrontCristian <cloud001@appfront.cloud>
2025-03-09 14:27:07 +0100

    Update config.js stage name param

--- www/src/config.js
+++ www/src/config.js
@@ -5,7 +5,8 @@ const config = {
   "aws_user_pools_web_client_id": "nl63ssoctq3pcbf7901sedjtd",     // CognitoClientID
   "api_base_url": "https://ggawl4trv1.execute-api.eu-north-1.amazonaws.com/prod", // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-thetrainingstack.auth.eu-north-1.amazoncognito.com", // CognitoDomainName
-  "redirect_url": "https://master.d1tgwb9esjzq6i.amplifyapp.com"  // AmplifyURL
+  "redirect_url": "https://master.d1tgwb9esjzq6i.amplifyapp.com",  // AmplifyURL
+  "STAGE_NAME_PARAM": "prod"
 };
 
 export default config;
