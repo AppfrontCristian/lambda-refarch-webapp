153d8a6dd3a238c3e1d5304aba2fc4783c1b0d3d
AppfrontCristian <cloud001@appfront.cloud>
2025-03-09 14:30:05 +0100

    Update config.js

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "nl63ssoctq3pcbf7901sedjtd",     // CognitoClientID
-  "api_base_url": "https://ggawl4trv1.execute-api.eu-north-1.amazonaws.com/prod", // TodoFunctionApi
+  "api_base_url": "https://ggawl4trv1.execute-api.eu-north-1.amazonaws.com/{StageNameParam}", // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-thetrainingstack.auth.eu-north-1.amazoncognito.com", // CognitoDomainName
   "redirect_url": "https://master.d1tgwb9esjzq6i.amplifyapp.com",  // AmplifyURL
   "STAGE_NAME_PARAM": "prod"
