76f4d5408ae69b7fe80b84eb10e43bb60508b868
AppfrontCristian <cloud001@appfront.cloud>
2025-03-09 14:02:01 +0100

    Update config.js

--- www/src/config.js
+++ www/src/config.js
@@ -1,23 +1,12 @@
 // You can obtain these values by running:
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
-// Configuration for connecting to AWS services
 
 const config = {
-  // Cognito settings
-  aws_user_pools_web_client_id: "nl63ssoctq3pcbf7901sedjtd", // Cognito User Pool Web Client ID
-  cognito_hosted_domain: "mytodoappdemo-thetrainingstack.auth.eu-north-1.amazoncognito.com", // Cognito Hosted UI Domain Name
-  cognito_id: "eu-north-1_Vy88xQDy9", // Cognito User Pool ID
-
-  // API Gateway URL with stage name placeholder
-  api_base_url: "https://ggawl4trv1.execute-api.eu-north-1.amazonaws.com/{StageNameParam}", // API Gateway URL
-
-  // Amplify app URL for redirect after authentication
-  redirect_url: "https://master.d1tgwb9esjzq6i.amplifyapp.com", // Amplify app URL for redirect
-
-  // Replace stage placeholder dynamically when using the API
-  getApiUrl: function(stageName) {
-    return this.api_base_url.replace("{StageNameParam}", stageName);
-  }
+  "aws_user_pools_web_client_id": "nl63ssoctq3pcbf7901sedjtd",     // CognitoClientID
+  "api_base_url": "https://ggawl4trv1.execute-api.eu-north-1.amazonaws.com/prod", // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-thetrainingstack.auth.eu-north-1.amazoncognito.com", // CognitoDomainName
+  "redirect_url": "https://master.d1tgwb9esjzq6i.amplifyapp.com"  // AmplifyURL
 };
 
 export default config;
+
